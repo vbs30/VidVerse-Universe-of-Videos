24474570ad2e2387c7f4ec244f4ac21edf018522
Vinayak Suryavanshi <vbs02002@gmail.com>
2025-04-18 22:49:59 +0530

    Refactor error handling in verifyJWT middleware to return ApiResponse for unauthorized requests

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.models.js"
 import { ApiError } from "../utils/apiError.js"
+import {ApiResponse} from "../utils/apiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import jwt from "jsonwebtoken";
 
@@ -8,7 +9,7 @@ export const verifyJWT = asyncHandler(async (req, _, next) => {
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "")
 
         if (!token) {
-            throw new ApiError(401, "Unauthorized request")
+            return new ApiResponse(200, "Unauthorized request")
         }
 
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
